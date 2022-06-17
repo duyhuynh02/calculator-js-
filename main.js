05c856f179c44d23bf0434c18fa32681e9475587
Dean <imnikz@gmail.com>
2022-06-17 14:25:21 +0700

    Add dot for float number

--- main.js
+++ main.js
@@ -36,10 +36,20 @@ let firstNumber = 0;
 let nextNumber = 0;
 let operator = '';
 let queue = [firstNumber];
+let isDot = false; 
+let countDot = 0; 
 
 const equalBtn = document.getElementById('equal');
 const clearBtn = document.getElementById('CLEAR'); 
 const deleteBtn = document.getElementById('DELETE');
+const dotBtn = document.getElementById('dot');
+
+dotBtn.addEventListener("click", () => {
+    isDot = true;
+    if (countDot < 1) {
+        displayValue.textContent += '.';
+    }
+})
 
 deleteBtn.addEventListener("click", () => {
     let lastElement = queue[queue.length - 1]; 
@@ -91,45 +101,64 @@ buttons.forEach((button) => {
         // If the value of button in number list: 
         let lastElement = queue[queue.length - 1]; 
         if (button.textContent in numList) {
-            if (typeof(lastElement) === 'number' && queue.length === 1) {
-                if (lastElement >= 0) {
-                    lastElement = lastElement * 10 + Number(button.textContent);
-                    displayValue.textContent = lastElement; 
-                    queue.pop();
-                    queue.push(lastElement)
+            if (isDot === false) {
+                if (typeof(lastElement) === 'number' && queue.length === 1) {
+                    if (lastElement >= 0) {
+                        lastElement = lastElement * 10 + Number(button.textContent);
+                        displayValue.textContent = lastElement; 
+                        queue.pop();
+                        queue.push(lastElement)
+                    }
+                    else {
+                        lastElement = Math.ceil(lastElement * 10) - Number(button.textContent);
+                        displayValue.textContent = lastElement; 
+                        queue.pop();
+                        queue.push(lastElement)
+                    }
                 }
                 else {
-                    lastElement = Math.ceil(lastElement * 10) - Number(button.textContent);
+                    if (typeof(lastElement) !== 'number'){
+                        queue.push(0);
+                        lastElement = queue[queue.length - 1];
+                    }  
+                    lastElement = lastElement * 10 + Number(button.textContent);
                     displayValue.textContent = lastElement; 
-                    queue.pop();
-                    queue.push(lastElement)
+                    queue.pop(); 
+                    queue.push(lastElement);
                 }
             }
             else {
-                if (typeof(lastElement) !== 'number'){
-                    queue.push(0);
-                    lastElement = queue[queue.length - 1];
-                }  
-                lastElement = lastElement * 10 + Number(button.textContent);
-                displayValue.textContent = lastElement; 
-                queue.pop(); 
-                queue.push(lastElement);
+                if (typeof(lastElement) === 'number') {
+                    if (countDot < 1) {
+                        lastElement = lastElement.toString() + '.' + button.textContent; 
+                        // displayValue.textContent += button.textContent
+                        countDot += 1;
+                    }
+                    else {
+                        lastElement = lastElement.toString() + button.textContent;
+                    }
+                    displayValue.textContent += button.textContent
+                    queue.pop();
+                    queue.push(Number(lastElement))
+                }
             }
         }
         else {
             if (button.textContent !== '=' && button.textContent !== 'CLEAR' 
-                    && button.textContent != 'DELETE') {
+                    && button.textContent != 'DELETE' && button.textContent != ".") {
                 operator = button.textContent;
                 if (typeof(queue[queue.length - 1]) === 'number' && queue.length == 1) {
                     queue.push(operator);
                 }
-                if (typeof(queue[queue.length - 1]) === 'number' && queue.length > 2){
+                else if (typeof(queue[queue.length - 1]) === 'number' && queue.length > 2){
                     const result = operate(queue[0], queue[1], queue[2]);
                     displayValue.textContent = result; 
                     queue.splice(0, 2); 
                     queue[0] = result;
                     queue.push(operator);
                 }
+                isDot = false; 
+                countDot = 0;
             }
         }
         console.log(queue);
